537cf6c3948b26d9195b2acfe2fe5223ce8b3913
Bryan <2022049@utsh.edu.mx>
2025-03-18 17:16:49 -0600

    fix[agregar grid ]

--- src/App.jsx
+++ src/App.jsx
@@ -175,25 +175,15 @@ function App() {
         </main>
       </div>
 
-      <div className="container mt-3">
-        <div className="row">
-          {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className="col-md-4 mb-3"
-              onClick={() => selectMovie(movie)}
-            >
-              <img
-                src={`${URL_IMAGE + movie.poster_path}`}
-                alt=""
-                height={600}
-                width="100%"
-              />
-              <h4 className="text-center">{movie.title}</h4>
-            </div>
-          ))}
-        </div>
-      </div>
+      <div className="movie-grid">
+  {movies.map((movie) => (
+    <div key={movie.id} className="movie-card" onClick={() => selectMovie(movie)}>
+      <img src={`${URL_IMAGE + movie.poster_path}`} alt={movie.title} />
+      <h4>{movie.title}</h4>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 }
